2b39597ada3e64c2c411155bd7f9810272697fc4
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2023-03-25 23:18:27 +0100

    Procedural test

--- CodeBlock2D/CodeBlock2D.cs
+++ CodeBlock2D/CodeBlock2D.cs
@@ -5,8 +5,14 @@ using Microsoft.Xna.Framework.Input;
 namespace CodeBlock2D;
 public class CodeBlock2D : Game
 {
+    private const double _minSpeed = 0.8d;
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
+    private double CircleSpeed = _minSpeed;
+    private Texture2D circle;
+    private Point CirclePos = new();
 
     public CodeBlock2D()
     {
@@ -18,33 +24,90 @@ public class CodeBlock2D : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+        Window.AllowUserResizing = true;
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        circle = Content.Load<Texture2D>("SmallCircle");
+        
         // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
+        double milliseconds = gameTime.ElapsedGameTime.TotalMilliseconds;
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        // get all keys with their states
+        KeyboardState keyboard = Keyboard.GetState();
+
+        // loop all pressed key
+        foreach (Keys key in keyboard.GetPressedKeys())
+        {
+            // update circle pos if needed
+            switch (key)
+            {
+                case Keys.A:
+                    CirclePos = new((int)(CirclePos.X - milliseconds * CircleSpeed) , CirclePos.Y);
+                    break;
+                case Keys.S:
+                    CirclePos = new(CirclePos.X, (int)(CirclePos.Y + milliseconds * CircleSpeed));
+                    break;
+                case Keys.D:
+                    CirclePos = new((int)(CirclePos.X + milliseconds * CircleSpeed), CirclePos.Y);
+                    break;
+                case Keys.W:
+                    CirclePos = new(CirclePos.X, (int)(CirclePos.Y - milliseconds * CircleSpeed));
+                    break;
+                case Keys.Space:
+                    CircleSpeed += 0.05 * CircleSpeed;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        CircleSpeed = SpeedCheck(CircleSpeed);
+        CirclePos = CheckForOutOfScreen(CirclePos, circle);
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.White);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(circle, CirclePos.ToVector2(), Color.White);
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private Point CheckForOutOfScreen(Point point, Texture2D texture)
+    {
+        if (point.X < 0) point.X = 0;
+        else if (point.X > Window.ClientBounds.Width - texture.Width) point.X = Window.ClientBounds.Width - texture.Width;
+
+        if (point.Y < 0) point.Y = 0;
+        else if (point.Y > Window.ClientBounds.Height - texture.Height) point.Y = Window.ClientBounds.Height - texture.Height;
+
+        return point;
+    }
+
+    private double SpeedCheck(double speed)
+    {
+        if(speed > 10)
+        {
+            return _minSpeed;
+        }
+        return speed;
     }
 }
