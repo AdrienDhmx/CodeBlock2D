f2953d94732a5b27cb673d7ff8ff840e89ca89d6
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2023-03-31 17:20:36 +0200

    Test map with 1D array

--- CodeBlock2D/CodeBlock2D.cs
+++ CodeBlock2D/CodeBlock2D.cs
@@ -5,8 +5,16 @@ using Microsoft.Xna.Framework.Input;
 namespace CodeBlock2D;
 public class CodeBlock2D : Game
 {
+    private const int BlocSize = 32;
+    private const int WindowWidth = 1152; // 36 blocs
+    private const int WindowHeight = 704; // 22 blocs
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
+    private Texture2D _dirtTexture;
+
+    private int[] Map;
 
     public CodeBlock2D()
     {
@@ -17,10 +25,12 @@ public class CodeBlock2D : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-        _graphics.PreferredBackBufferWidth = 1152;
-        _graphics.PreferredBackBufferHeight = 704;
+        // fix the window size
+        _graphics.PreferredBackBufferWidth = WindowWidth;
+        _graphics.PreferredBackBufferHeight = WindowHeight;
         _graphics.ApplyChanges();
+
+        Map = CreateMap();
 
         base.Initialize();
     }
@@ -28,8 +38,7 @@ public class CodeBlock2D : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        _dirtTexture = Content.Load<Texture2D>("dirt");
     }
 
     protected override void Update(GameTime gameTime)
@@ -46,8 +55,57 @@ public class CodeBlock2D : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        // col and line never change as the window size is fixed, they could be in a global variable
+        int col = WindowWidth / BlocSize;
+        int line = WindowHeight / BlocSize;
+        for (int l = 0; l < line; l++)
+        {
+            for (int c = 0; c < col; c++)
+            {
+                if (Map[l * col + c] == 1)
+                {
+                    _spriteBatch.Draw(_dirtTexture, new Rectangle(BlocSize*c, BlocSize * l, _dirtTexture.Width, _dirtTexture.Height), Color.White);
+                }
+            }
+        }
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    protected override void UnloadContent()
+    {
+        // free all memory
+        _dirtTexture.Dispose();
+        _spriteBatch.Dispose();
+        _graphics.Dispose();
+
+        base.UnloadContent();
+    }
+
+    private static int[] CreateMap()
+    {
+        int col = WindowWidth / BlocSize;
+        int line = WindowHeight / BlocSize;
+
+        int[] map = new int[col * line];
+        for (int l = 0; l < line; l++)
+        {
+            for (int c = 0; c < col; c++)
+            {
+                if(l < line/2) // start drawing dirt block at half the screen height
+                {
+                    map[l * col + c] = 0;
+                }
+                else
+                {
+                    map[l * col + c] = 1;
+                }
+            }
+        }
+        return map;
     }
 }
